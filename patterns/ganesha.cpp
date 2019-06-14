b595f3e00698e3687e4376c8f420bbc25580f015
Apaar Kamal <apaarsaxena@gmail.com>
2019-06-14 22:59:22 +0530

    Update ganesha.cpp

--- patterns/ganesha.cpp
+++ patterns/ganesha.cpp
@@ -1,10 +1,6 @@
 #include <iostream>
 using namespace std;
 int main() {
-        #ifndef ONLINE_JUDGE
-        freopen("input.txt", "r", stdin);
-        freopen("output.txt", "w", stdout);
-        #endif
     int n,mid;
     cin>>n;
     // for middle rows and colums
