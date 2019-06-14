e45ca4d492cc4ec03d6b4bb070b5f1a550b288af
apaarkamal <apaarsaxena@gmail.com>
2019-06-14 19:18:40 +0530

    14/06

--- patterns/ganesha.cpp
+++ patterns/ganesha.cpp
@@ -1,13 +1,18 @@
 #include <iostream>
 using namespace std;
 int main() {
+        #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+        #endif
     int n,mid;
     cin>>n;
+    // for middle rows and colums
     mid=(n+1)/2;
     for(int i=1;i<=n;i++){
         for(int j=1;j<=n;j++){
-            if(i==1){
-                if(j==1){
+            if(i==1){// for 1st row 
+                if(j==1||j>=mid){
                     cout<<"*";
                 }
                 else if(j>=mid){
@@ -17,21 +22,18 @@ int main() {
                     cout<<" ";
                 }
             }
-            else if(i==mid){
+            else if(i==mid){// for middle row
                 cout<<"*";
             }
-            else if(i==n){
-                if(j==n){
-                    cout<<"*";
-                }
-                else if(j<=mid){
+            else if(i==n){// for last row
+                if(j==n||j<=mid){
                     cout<<"*";
                 }
                 else{
                     cout<<" ";
                 }
             }
-            else{
+            else{// for rest of the rows
                 if(i<mid){
                     if(j==1||j==mid){
                         cout<<"*";
