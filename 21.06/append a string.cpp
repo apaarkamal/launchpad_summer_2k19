4ecdf51d83b9127199e4560ada4b3095c44425ad
apaarkamal <apaarsaxena@gmail.com>
2019-06-21 14:27:56 +0530

    21/06

---
+++ 21.06/append a string.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+    //  code starts
+    string s,t;
+    cin>>s>>t;
+    s+=t;
+    cout<<s;
+}
+        
