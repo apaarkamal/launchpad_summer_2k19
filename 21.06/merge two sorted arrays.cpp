649441244c6737d168ef3db9756acd4127464820
apaarkamal <apaarsaxena@gmail.com>
2019-06-21 13:12:21 +0530

    21/06

---
+++ 21.06/merge two sorted arrays.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+    //  code starts
+    int i,j,n,m;
+    cin>>n;
+    int arr1[n];
+    for(i=0;i<n;i++){
+        cin>>arr1[i];                   
+    }
+    cin>>m;
+    int arr2[m];
+    for(i=0;i<m;i++){
+        cin>>arr2[i];                   
+    }
+    int k=m+n;
+    int arr3[k];
+    i=0;j=0;
+    int kk=0;
+    while(i<n&&j<m){
+        if(arr1[i]<arr2[j]){
+            arr3[kk]=arr1[i];
+            kk++;
+            i++;
+        }
+        else{
+            arr3[kk]=arr2[j];
+            kk++;
+            j++;
+        }
+    }
+    while(i<n){
+        arr3[kk]=arr1[i];
+        kk++;i++;
+    }
+    while(j<m){
+        arr3[kk]=arr2[j];
+        kk++;j++;
+    }
+    for(i=0;i<k;i++){
+        cout<<arr3[i]<<" ";            
+    }
+}
+        
---
+++ 21.06/seive of eratosthenes.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    //  code starts
+    int N=1000001;
+    int seivearray[N],i,j;
+    for(i=0;i<N;i++){
+        seivearray[i]=1;           
+    }
+    // 1 means prime
+    // 0 means not prime 
+    seivearray[0]=0;seivearray[1]=0;
+    for(i=2;i<N;i++){// whose multiples to be cancelled
+        if(seivearray[i]==1){
+            // if the number is prime
+            for(j=i*2;j<N;j+=i){// multiples of prime
+                seivearray[j]=0;           
+            }       
+        }
+    }
+    // copy primes to the new array
+    // here primes[i]= ith prime number
+    int primes[N],k=1;
+    for(i=2;i<N;i++){
+        if(seivearray[i]==1){
+            primes[k]=i;
+            k++;
+        }                
+    }
+    int testcase;
+    cin>>testcase;
+    while(testcase--){// run test cases
+        int n;
+        cin>>n;
+        cout<<primes[n]<<'\n';
+    }
+}
+        
