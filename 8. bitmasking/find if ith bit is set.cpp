3e5338e47103c824284e192e5d8398032ebb3449
apaarkamal <apaarsaxena@gmail.com>
2019-07-06 18:49:48 +0530

    added codes

---
+++ 8. bitmasking/find if ith bit is set.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+    //  code starts
+    int n,k;
+    // code to find if the kth bit of a number is set or not
+    cin>>n;
+    cout<<( ((1<<k) & n)?"set":"not set");
+}
+        
