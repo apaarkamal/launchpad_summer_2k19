21e308806a6579adebb83367a3a84356a223db11
apaarkamal <apaarsaxena@gmail.com>
2019-07-07 19:31:52 +0530

    added codes

--- 08. bitmasking/find if ith bit is set.cpp
+++ 08. bitmasking/find if ith bit is set.cpp
@@ -11,7 +11,7 @@ int main()
     //  code starts
     int n,k;
     // code to find if the kth bit of a number is set or not
-    cin>>n;
+    cin>>n>>k;
     cout<<( ((1<<k) & n)?"set":"not set");
 }
         
