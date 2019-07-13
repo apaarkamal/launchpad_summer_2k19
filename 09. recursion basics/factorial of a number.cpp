71721928d5b9ed38ba441081513fc5ba78abf06d
apaarkamal <apaarsaxena@gmail.com>
2019-07-13 14:06:42 +0530

    added codes

---
+++ 09. recursion basics/factorial of a number.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int fact(int n){
+    if(n==1){
+        return 1;
+    }
+    return n*fact(n-1);
+}
+
+int main()
+{
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+    //  code starts
+    int n;
+    cin>>n;
+    cout<<fact(n);
+}
+        
