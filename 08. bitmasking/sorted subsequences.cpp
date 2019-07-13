a6def9442f26639a22869e942354aa850293bd8c
apaarkamal <apaarsaxena@gmail.com>
2019-07-13 14:09:43 +0530

    added codes

---
+++ 08. bitmasking/sorted subsequences.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	#endif
+	//  code starts
+	int t;cin>>t;while(t--){
+		string s;
+		cin>>s;
+		int i,j,n=s.size();
+		string s_ar[1<<n];
+		for(i=0;i<(1<<n);i++){
+		 	for(j=0;j<n;j++){
+		 	    if((i&(1<<j))){
+		 	    	s_ar[i]+=s[j];
+		 	    }       
+		 	}       
+		}
+		sort(s_ar,s_ar+(1<<n));
+		for(i=0;i<(1<<n);i++){
+		 	cout<<s_ar[i]<<'\n';       
+		}
+	}
+}
+        
---
+++ 14. stacks and queues/rainwater trapping and harvesting.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	#endif
+	//  code starts
+	int i,n;
+	cin>>n;
+	int a[n],left[n],right[n];
+	for(i=0;i<n;i++){
+	 	cin>>a[i];                   
+	}
+	// to the left max element
+	left[0]=a[0];
+	for(i=1;i<n;i++){
+	 	left[i]=max(left[i-1],a[i]);                
+	}
+	// to the right max element
+	right[n-1]=a[n-1];
+	for(i=n-2;i>=0;i--){
+	 	right[i]=max(right[i+1],a[i]);        
+	}
+	int ans=0;
+	for(i=1;i<n-1;i++){
+	 	ans+=min(left[i],right[i])-a[i];     
+	}
+	cout<<ans<<'\n';
+}
+        
