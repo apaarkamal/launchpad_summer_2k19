86c99b0f50a469ec20067165e3dbfd9d09c52e93
apaarkamal <apaarsaxena@gmail.com>
2019-07-17 16:16:53 +0530

    added codes

---
+++ 00. Adhoc/use of stl map.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define int long long int
+#define ld long double
+#define F first
+#define S second
+#define pb push_back
+
+int32_t main()
+{
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	#endif
+	//  code starts
+	// map<int,int> mp;
+	// mp[3]=5;
+	// mp[2]=1;
+	// mp[1]=1;
+	// mp.insert({2,1});
+	
+	map<string,int> mp;
+	mp["apaar"]=20;
+	mp["archit"]=18;
+	for(auto x:mp){
+		cout<<x.F<<" "<<x.S<<'\n';
+	}
+}
---
+++ 00. Adhoc/ways of iteration.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define int long long int
+#define ld long double
+#define F first
+#define S second
+#define pb push_back
+
+int32_t main()
+{
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	#endif
+	//  code starts
+	vector<int> v;
+	v.pb(3);
+	v.pb(2);
+	for(int i=0;i<v.size();i++){
+		cout<<v[i]<<'\n';
+	}
+	vector<int>::iterator it;
+	for(it=v.begin();it!=v.end();it++){
+		cout<<*it<<'\n';
+	}
+	for(int x:v){
+		cout<<x<<'\n';
+	}
+	for(auto x:v){
+		cout<<x<<'\n';
+	}
+}
