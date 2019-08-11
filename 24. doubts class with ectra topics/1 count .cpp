ea8496c57f01d341e5b1db0293d254b437752cd5
apaarkamal <apaarsaxena@gmail.com>
2019-08-11 16:10:06 +0530

    latest

---
+++ 24. doubts class with ectra topics/1 count .cpp
@@ -0,0 +1,58 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define int long long int
+#define ld long double
+#define F first
+#define S second
+#define P pair<int,int>
+#define pb push_back
+
+const int N=100005;
+
+int32_t main()
+{
+    ios_base:: sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    // int t;cin>>t;while(t--)
+    {
+        int i,j,k,n,m,ans=0,cnt=0,sum=0;
+        cin>>n>>k;
+        int a[n];
+        for(i=0;i<n;i++){
+            cin>>a[i];                   
+        }
+        int lf=-1,rt=-1;
+        i=0;j=0;
+        while(i<n){
+            while(j<n){
+                if(a[j]==0){
+                    if(k>0){
+                        k--;
+                        j++;
+                    }
+                    else{
+                        break;
+                    }
+                }
+                else{
+                    j++;
+                }
+            }
+            if(ans < j-i){
+                ans=j-i;
+                lf=i;rt=j;
+            }
+            if(a[i]==0) k++;
+            i++;
+        }
+        cout<<ans<<'\n';
+        for(i=lf;i<rt;i++){
+            a[i]=1;                      
+        }
+        for(i=0;i<n;i++){
+            cout<<a[i]<<" ";                   
+        }
+    }
+}   
---
+++ 24. doubts class with ectra topics/fenwick tree.cpp
@@ -0,0 +1,54 @@
+#include<bits/stdc++.h>
+
+#define int long long int
+#define F first
+#define S second
+#define pb push_back
+
+using namespace std;
+
+const int N=100005;
+
+class BIT
+{
+public:
+    int a[N];
+    void add(int x, int val)
+    {
+        x++;
+        while (x < N)
+        {
+            a[x] += val;
+            x += (x&-x);
+        }
+    }
+    int sum(int x)
+    {
+        int ret = 0;
+        x++;
+        while (x)
+        {
+            ret += a[x];
+            x -= (x&-x);
+        }
+        return ret;
+    }
+}bit;
+
+int32_t main()
+{
+    ios_base:: sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    {
+        int i,j,k,n,m,ans=0,cnt=0,sum=0;  
+        cin>>n;
+        int a[n];
+        for(i=0;i<n;i++){
+            cin>>a[i];
+            bit.add(i,a[i]);
+            sum=bit.sum(i);
+            cout<<sum<<'\n';
+        }  
+        
+    }
+}
